a91b13509e632c26c107a3b0552e2a2825f327e1
Андрей Масяго <you@example.com>
2022-08-11 10:45:18 +0700

    Added settings. Some bugs fixed. Refactored

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from dadata import Dadata
 
+from enum import Enum
+
 import json
 import sqlite3
 import logging
+
+DEFAULT_URL = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address'
+DEFAULT_LANGUAGE = 'ru'
 
 
 def safe_cast(val, to_type, default=None):
@@ -12,7 +17,7 @@ def safe_cast(val, to_type, default=None):
         return default
 
 
-class Klass:
+class Suggestion:
     def __init__(self):
         self.id = 0
         self.value = ""
@@ -24,12 +29,38 @@ class Klass:
         print("unrestricted_value: ", self.unrestricted_value)
 
 
+# TODO: Разберись со своим языком. ЧТО ТЫ НЕСЕШЬ
+class Language(Enum):
+    RU = 1
+    EN = 2
+
+
+class Settings:
+    def __init__(self, url=None, apikey=None, language=None):
+        self.id = -1
+        if url is None:
+            self.url = DEFAULT_URL
+        else:
+            self.url = url
+
+        self.apikey = apikey
+
+        self.language = Language.RU
+
+    def print(self):
+        print('Settings:')
+        print('   \ \ \ \___ id =', self.id)
+        print('    \ \ \____ url =', self.url)
+        print('     \ \_____ apikey =', self.apikey)
+        print('      \______ language = ', self.language)
+        print()
+
+
 if __name__ == '__main__':
     conn = sqlite3.connect('settings.db')
     cursor = conn.cursor()
 
-    testing_marker = True
-
+    testing_marker = False
     if testing_marker:
         # TEST usage only. Delete before real using. Dropping and creating settings table
         try:
@@ -37,7 +68,7 @@ if __name__ == '__main__':
             cursor.execute(drop_table_query)
             conn.commit()
 
-    # TODO: apikey NOT NULL ?
+            # TODO: apikey NOT NULL ?
             create_table_query = "CREATE TABLE IF NOT EXISTS settings(\
                 userid INTEGER PRIMARY KEY AUTOINCREMENT,\
                 url TEXT DEFAULT 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address' NOT NULL,\
@@ -51,27 +82,41 @@ if __name__ == '__main__':
             logging.exception("Error occurred while recreating settings table")
 
         try:
-            fill_table_query = "INSERT INTO settings(url) VALUES('url');"
+            fill_table_query = "INSERT INTO settings(apikey) VALUES('c8c03d3a7321ea0ba36ebe1e46de06b7083d9d5e');"
             cursor.execute(fill_table_query)
             conn.commit()
         except sqlite3.Error as err:
             logging.exception("Error occurred while filling settings table")
+
+    settings = Settings(url='google.com')
+    settings.print()
 
     try:
         select_everything_from_settings_table_query = "SELECT * FROM settings;"
         cursor.execute(select_everything_from_settings_table_query)
         conn.commit()
         cur_settings = cursor.fetchone()
-        print("settings")
+        print("from settings table")
         print(cur_settings)
         print()
+
+        if len(cur_settings) == 4:
+            settings.id = cur_settings[0]
+            settings.url = cur_settings[1]
+            settings.apikey = cur_settings[2]
+            settings.language = cur_settings[3]
+
+        settings.print()
+
     except sqlite3.Error as err:
         logging.exception("Error occurred while selecting from settings table")
+
+    get_token_query = "SELECT * FROM WHERE userid="
 
     token = "c8c03d3a7321ea0ba36ebe1e46de06b7083d9d5e"
     dadata = Dadata(token)
 
-    # print("balans = ", dadata.get_balance())
+    # print("balance = ", dadata.get_balance())
 
     print("Che nada")
     # query = input()
@@ -87,26 +132,26 @@ if __name__ == '__main__':
 
     parsed = json.loads(jsonString)
 
-    dict_supa_parsed = dict()
+    dict_super_parsed = dict()
 
-    obj = Klass()
+    obj = Suggestion()
     j = 0
     for p in result:
-        tmp_obj = Klass()
+        tmp_obj = Suggestion()
         tmp_obj.value = p.get('value')
         tmp_obj.unrestricted_value = p.get('unrestricted_value')
         tmp_obj.id = j
 
-        dict_supa_parsed[j] = tmp_obj
+        dict_super_parsed[j] = tmp_obj
         j = j + 1
 
     print()
     print("DICT")
 
-    for key in dict_supa_parsed:
+    for key in dict_super_parsed:
         print("=====       ", key, "        =====")
-        print("value: ", dict_supa_parsed[key].value)
-        print("unrestricted_value: ", dict_supa_parsed[key].unrestricted_value)
+        print("value: ", dict_super_parsed[key].value)
+        print("unrestricted_value: ", dict_super_parsed[key].unrestricted_value)
 
     print()
 
@@ -119,8 +164,8 @@ if __name__ == '__main__':
     # safe_cast('tst', int) # will return None
     # safe_cast('tst', int, 0) # will return 0
 
-    while asked is None or dict_supa_parsed.get(asked) is None:
-        print("Choose from [ 0 - ", len(dict_supa_parsed) - 1, "] array.")
+    while asked is None or dict_super_parsed.get(asked) is None:
+        print("Choose from [ 0 - ", len(dict_super_parsed) - 1, "] array.")
         # asked = input()
         asked = 2
         asked = safe_cast(asked, int)
@@ -128,7 +173,7 @@ if __name__ == '__main__':
     # ПОЛУЧИЛИ ТОЧНЫЙ ЗАПРОС
 
     print("Запросили", asked)
-    query = dict_supa_parsed[asked].unrestricted_value
+    query = dict_super_parsed[asked].unrestricted_value
 
     result2 = dadata.suggest("address", query, 1)
 
